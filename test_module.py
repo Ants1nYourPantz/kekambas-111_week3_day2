ced67d72495c6542469ad04ae6c26f09791eeae0
Anthony Romano <dentlabpa@gmail.com>
2023-03-01 09:29:00 -0500

    updated homework

---
+++ test_module.py
@@ -0,0 +1,21 @@
+def circle():
+    from math import pi
+
+    radius = 7
+
+    area = pi * radius **2
+
+    print(area)
+circle()
+
+
+def triangle():
+    import math
+
+    a = 3
+    b = 4
+
+    c = math.sqrt(a**2 + b**2)
+
+    print(c)
+triangle()
